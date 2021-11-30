be352351b8dea491ace1b67265116dc428621753
MARY Alexandre <alexandre.mary@meteo.fr>
2021-11-30 15:33:05 +0000

    runs: stop if ciboulai setup fails

--- src/runs/RUN_XP.sh
+++ src/runs/RUN_XP.sh
@@ -3,13 +3,20 @@
 
 # 0. Setup ciboulai XP
 ./0.setup_ciboulai.sh
-# 1. Build
-./1.packbuild.sh
-if [ "$?" == "1" ];then
-  echo "Build failed: cannot run tests. Exit."
-  exit 1
+ok=$?
+if [ "$ok" != "0" ];then
+  echo "Ciboulai setup failed: fix before running tests. Exit."
+  exit $ok
 else
-  echo "Let's go for the tests !"
-# 2. Run tests
-  ./2.tests.sh
+  echo "Let's go for the build !"
+  # 1. Build
+  ./1.packbuild.sh
+  if [ "$?" == "1" ];then
+    echo "Build failed: cannot run tests. Exit."
+    exit 1
+  else
+    echo "Let's go for the tests !"
+  # 2. Run tests
+    ./2.tests.sh
+  fi
 fi
