1300a93439f0ca9f2df18df195c5d54c90ed5351
MARY Alexandre <alexandre.mary@meteo.fr>
2023-03-03 12:00:52 +0000

    TEMPORARY: fix path to eccodes samples; should be properly fixed in Vortex

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -27,6 +27,8 @@ class DavaiJobAssistantPlugin(JobAssistantPlugin):
         t.env.MPIAUTOCONFIG = self.masterja.conf.mpiautoconfig
         t.env.DAVAI_SERVER = self.masterja.conf.davai_server
         t.env.EC_MEMINFO = '0'  # FIXME: without, some exec crash at EC_MEMINFO setup... -> fixed in CY49 !
+        if 'eccodes_samples_path' in self.masterja.conf:
+            t.env.ECCODES_SAMPLES_PATH = self.masterja.conf.eccodes_samples_path
         # set token from file if not in env
         ciboulai_token = None
         if 'CIBOULAI_TOKEN' not in t.env:
