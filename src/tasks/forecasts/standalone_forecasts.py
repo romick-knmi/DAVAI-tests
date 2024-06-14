f03811a06ca3c5c5b24aa59360b602e5b3c63f52
Michael Glinton <michael.glinton@meteo.fr>
2024-06-14 14:14:41 +0000

    Reference for CY48T1_op1.16

--- src/tasks/forecasts/standalone_forecasts.py
+++ src/tasks/forecasts/standalone_forecasts.py
@@ -21,11 +21,15 @@ def setup(t, **kw):
             loopconf='compilation_flavours',
             loopsuffix='.{}',
             nodes=[
-                Family(tag='ifs', ticket=t, on_error='delayed_fail', nodes=[
-                    Family(tag='global21', ticket=t, nodes=[
-                        StandaloneIFSForecast(tag='forecast-ifs-global21', ticket=t, **kw),
-                        ], **kw),
-                    ], **kw),
+                # Michael G 14/06/24: cy48t1_op1.16 was non-reproducable for the test
+                # standalone-forecast-ifs-global21.impiifc2018.2ys, but this branch
+                # is a dead end that should not be used for IFS, so this family of tests
+                # can be disactivated.
+                # Family(tag='ifs', ticket=t, on_error='delayed_fail', nodes=[
+                #     Family(tag='global21', ticket=t, nodes=[
+                #         StandaloneIFSForecast(tag='forecast-ifs-global21', ticket=t, **kw),
+                #         ], **kw),
+                #     ], **kw),
                 Family(tag='arpege', ticket=t, on_error='delayed_fail', nodes=[
                     Family(tag='globaltst149c24', ticket=t, nodes=[
                         StandaloneArpegeForecast(tag='forecast-arpege-globaltst149c24', ticket=t, **kw),
