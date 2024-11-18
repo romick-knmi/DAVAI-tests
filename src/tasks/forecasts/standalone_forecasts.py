9c1c7ed120eb148df96f540181d9125e4f3db4f0
Christopher Romick (KNMI) <chris.romick@knmi.nl>
2024-11-18 10:59:30 +0000

    Updating the standalone forecast script so that only the arome forecast is run

--- src/tasks/forecasts/standalone_forecasts.py
+++ src/tasks/forecasts/standalone_forecasts.py
@@ -22,32 +22,42 @@ def setup(t, **kw):
             loopconf='compilation_flavours',
             loopsuffix='.{}',
             nodes=[
-                Family(tag='ifs', ticket=t, on_error='delayed_fail', nodes=[
-                    Family(tag='global21', ticket=t, nodes=[
-                        StandaloneIFSForecast(tag='forecast-ifs-global21', ticket=t, **kw),
-                        ], **kw),
-                    ], **kw),
-                Family(tag='arpege', ticket=t, on_error='delayed_fail', nodes=[
-                    Family(tag='globaltst149c24', ticket=t, nodes=[
-                        StandaloneArpegeForecast(tag='forecast-arpege-globaltst149c24', ticket=t,
-                                                 on_error='delayed_fail', **kw),
-                        StandaloneArpegeForecast(tag='forecast-arpege_nosfx-globaltst149c24', ticket=t, **kw),
-                        ], **kw),
-                    ], **kw),
+#                Family(tag='ifs', ticket=t, on_error='delayed_fail', nodes=[
+#                    Family(tag='global21', ticket=t, nodes=[
+#                        StandaloneIFSForecast(tag='forecast-ifs-global21', ticket=t, **kw),
+#                        ], **kw),
+#                    ], **kw),
+#                Family(tag='arpege', ticket=t, on_error='delayed_fail', nodes=[
+#                    Family(tag='globaltst149c24', ticket=t, nodes=[
+#                        StandaloneArpegeForecast(tag='forecast-arpege-globaltst149c24', ticket=t,
+#                                                 on_error='delayed_fail', **kw),
+#                        StandaloneArpegeForecast(tag='forecast-arpege_nosfx-globaltst149c24', ticket=t, **kw),
+#                        ], **kw),
+#                    ], **kw),
                 Family(tag='arome', ticket=t, on_error='delayed_fail', nodes=[
                     Family(tag='corsica2500', ticket=t, nodes=[
                         StandaloneAromeForecast(tag='forecast-arome-corsica2500', ticket=t, **kw),
                         ], **kw),
                     ], **kw),
-                Family(tag='alaro', ticket=t, on_error='delayed_fail', nodes=[
-                    Family(tag='antwrp1300', ticket=t, on_error='delayed_fail', nodes=[
-                        StandaloneAlaroForecast(tag='forecast-alaro0-antwrp1300', ticket=t, on_error='delayed_fail', **kw),
-                        StandaloneAlaroForecast(tag='forecast-alaro1-antwrp1300', ticket=t, on_error='delayed_fail', **kw),
-                        ], **kw),
-                    Family(tag='chmh2325', ticket=t, nodes=[
-                        StandaloneAlaroForecast(tag='forecast-alaro1_sfx-chmh2325', ticket=t, **kw),
-                        ], **kw),
-                    ], **kw),
+#~~~~~~~~~~~~~~~~~~~
+# romick-knmi : 2024-Nov : attempting to start a harmonie arome forecast - this currently will only try to load from 4dvarfr Arpege - might not work properly
+#                Family(tag='harmonie-arome', ticket=t, on_error='delayed_fail', nodes=[
+#                    Family(tag='corsica2500', ticket=t, nodes=[
+#                        StandaloneAromeForecast(tag='forecast-arome-corsica2500', ticket=t, **kw),
+#                        StandaloneAromeForecast(tag='forecast-arome-nl2500', ticket=t, on_error='delayed_fail', **kw),
+#                        ], **kw),
+#                    ], **kw),
+#
+#~~~~~~~~~~~~~~~~~~~
+#                Family(tag='alaro', ticket=t, on_error='delayed_fail', nodes=[
+#                    Family(tag='antwrp1300', ticket=t, on_error='delayed_fail', nodes=[
+#                        StandaloneAlaroForecast(tag='forecast-alaro0-antwrp1300', ticket=t, on_error='delayed_fail', **kw),
+#                        StandaloneAlaroForecast(tag='forecast-alaro1-antwrp1300', ticket=t, on_error='delayed_fail', **kw),
+#                        ], **kw),
+#                    Family(tag='chmh2325', ticket=t, nodes=[
+#                        StandaloneAlaroForecast(tag='forecast-alaro1_sfx-chmh2325', ticket=t, **kw),
+#                        ], **kw),
+#                    ], **kw),
                 ], **kw),
         ],
     )
